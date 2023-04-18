b9cb2fe24119572fd88a02549f4b1d82f4606038
JaderBasili <jabasili@gmail.com>
2023-04-18 10:46:49 -0300

    first commit

---
+++ cypress/integration/Login.spec.js
@@ -0,0 +1,45 @@
+describe('Fazer login e logout', () => {
+    // Armazena as variaveis de elementos usados em todo o teste
+    before(() => {
+        Cypress.env('TextoTelaInicial', 'div[class=login_logo]')
+        Cypress.env('TelaProdutos', 'span[class="title"]')
+    })
+    it('Visitar a Pagina', () => {
+        cy.visit('')
+        // Verifica se o texto "Swag Labs" é exibido na página inicial 
+        cy.get(Cypress.env('TextoTelaInicial'))
+        .should('be.visible')
+        .and('have.text', 'Swag Labs')
+    })
+    it('Fazer login', () => {
+        // Executa o comando personalizado "DadosLogin" para inserir as informações de login
+        cy.DadosLogin()     
+        cy.get('@User')
+        .should('be.visible')
+        cy.get('@Senha')
+        .should('be.visible')
+    })            
+
+    it('Clicar no botão entrar', () => {
+        // Executa o comando personalizado "ClicarBotaoLogin" para clicar no botão de login
+        cy.ClicarBotaoLogin()
+        // Verifica se o texto "Products" é exibido após o login bem-sucedido
+        cy.get(Cypress.env('TelaProdutos'))
+        .should('be.visible')
+        .and('have.text', 'Products')
+    })
+
+    it('Fazer logout', () => {
+        // Executa o comando personalizado "ClicarOpenMenu" para abrir o menu de usuário
+        cy.ClicarOpenMenu()
+        cy.get('@OpenMenu')
+        .should('be.visible')
+        // Executa o comando personalizado "FazerLogout" para fazer logout do usuário
+        cy.FazerLogout()
+        // Verifica se o usuário foi redirecionado para a página inicial após o logout
+        cy.get(Cypress.env('TextoTelaInicial'))
+        .should('be.visible')
+        .and('have.text', 'Swag Labs')
+    })
+
+})
---
+++ cypress/support/commands.js
@@ -0,0 +1,20 @@
+
+Cypress.Commands.add('DadosLogin', () => {
+    cy.fixture('Login').then((Login) =>{
+    cy.get('input[id=user-name]').as('User').type(Login.email)
+    cy.get('input[id=password]').as('Senha').type((Login.senha), { log: false })
+    })
+})
+
+Cypress.Commands.add('ClicarBotaoLogin', () => {
+    cy.get('input[id=login-button]').as('BotaoLogin').click()
+    cy.get('div[id=page_wrapper]').as('PaginaLogada')
+})
+
+Cypress.Commands.add('ClicarOpenMenu', () => {
+    cy.get('button[id=react-burger-menu-btn]').as('OpenMenu').click()
+})
+
+Cypress.Commands.add('FazerLogout', () => {
+    cy.get('a[id=logout_sidebar_link]').as('Logout').click()
+})
