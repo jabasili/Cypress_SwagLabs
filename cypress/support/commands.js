052b6039dc70e089f415258ae284470d65fa990e
JaderBasili <jabasili@gmail.com>
2023-04-18 15:30:32 -0300

    ajuste de comandos personalizados

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,30 +1,53 @@
 Cypress.Commands.add('DadosLoginErrado', () => {
-    cy.fixture('Login').then((Login) =>{
-    cy.get('input[id=user-name]').as('User').type(Login.email)
-    cy.get('input[id=password]').as('Senha').type((Login.senhaErrada), { log: false })
-    cy.get('input[id=login-button]').as('BotaoLogin').click()
-    cy.get('@User').clear()
-    cy.get('@Senha').clear()
+    cy.fixture('Login')
+        .then((Login) =>{
+    cy.get('input[id=user-name]')
+        .as('User')
+        .type(Login.email)
+    cy.get('input[id=password]')
+        .as('Senha')
+        .type((Login.senhaErrada), { log: false })
+    cy.get('input[id=login-button]')
+        .as('BotaoLogin')
+        .click()
+    cy.get('@User')
+        .clear()
+    cy.get('@Senha')
+        .clear()
     })
 })
 
 Cypress.Commands.add('DadosLogin', () => {
-    cy.fixture('Login').then((Login) =>{
-    cy.get('input[id=user-name]').as('User').type(Login.email)
-    cy.get('input[id=password]').as('Senha').type((Login.senha), { log: false })
+    cy.fixture('Login')
+        .then((Login) =>{
+    cy.get('input[id=user-name]')
+        .as('User')
+        .type(Login.email)
+    cy.get('input[id=password]')
+        .as('Senha')
+        .type((Login.senha), { log: false })
     })
 })
 
 Cypress.Commands.add('ClicarBotaoLogin', () => {
-    cy.get('input[id=login-button]').as('BotaoLogin').click()
-    cy.get('div[id=page_wrapper]').as('PaginaLogada')
+    cy.get('input[id=login-button]')
+        .as('BotaoLogin')
+        .click()
+    cy.get('div[id=page_wrapper]')
+        .as('PaginaLogada')
 })
 
 Cypress.Commands.add('ClicarOpenMenu', () => {
-    cy.get('button[id=react-burger-menu-btn]').as('OpenMenu').click()
+    cy.get('button[id=react-burger-menu-btn]')
+        .as('OpenMenu')
+        .click()
 })
 
 Cypress.Commands.add('FazerLogout', () => {
-    cy.get('a[id=logout_sidebar_link]').as('Logout').click()
+    cy.get('#logout_sidebar_link')
+        .should('be.visible')
+    cy.get('a[id=logout_sidebar_link]')
+        .as('Logout')
+        .click()
 })
 
